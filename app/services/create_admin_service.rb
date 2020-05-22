7ffaa70544fa0e6b6f89afafddad9fc4de8cdf0d
iarunpaul <iarunpaul@yahoo.com>
2020-05-22 07:32:26 +0000

    Added 2 admins to seed

--- app/services/create_admin_service.rb
+++ app/services/create_admin_service.rb
@@ -1,9 +1,14 @@
 class CreateAdminService
   def call
-    user = User.find_or_create_by!(email: Rails.application.secrets.admin_email) do |user|
+    User.find_or_create_by!(email: Rails.application.secrets.admin_email1) do |user|
+        user.password = Rails.application.secrets.admin_password
+        user.password_confirmation = Rails.application.secrets.admin_password
+        user.role = 'admin'
+    User.find_or_create_by!(email: Rails.application.secrets.admin_email2) do |user|
         user.password = Rails.application.secrets.admin_password
         user.password_confirmation = Rails.application.secrets.admin_password
         user.admin!
+      end
       end
   end
 end
